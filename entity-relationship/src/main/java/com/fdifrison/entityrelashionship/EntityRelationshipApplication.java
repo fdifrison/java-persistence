513b243f9e5e420081b8d71753e5162ad6722907
fdifrison <ing.giovanni.frison@gmail.com>
2024-12-19 13:18:38 +0100

    Working on transaction logging

--- entity-relationship/src/main/java/com/fdifrison/entityrelashionship/EntityRelationshipApplication.java
+++ entity-relationship/src/main/java/com/fdifrison/entityrelashionship/EntityRelationshipApplication.java
@@ -1,11 +1,8 @@
 package com.fdifrison.entityrelashionship;
 
-import com.fdifrison.entityrelashionship.entities.PostService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -15,12 +12,4 @@ public class EntityRelationshipApplication {
         SpringApplication.run(EntityRelationshipApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner(PostService postService) {
-        return args -> {
-            //            var postId = postService.savePost().getId();
-            //            var postCommentId = postService.addComment(postId).getId();
-            //            postService.unlinkComment(postCommentId);
-        };
-    }
 }
