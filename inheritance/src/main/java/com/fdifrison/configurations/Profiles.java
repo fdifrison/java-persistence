39a6ee802d1a026069cbbe9b02be83d2eb1a4f76
giovanni frison <giovanni.frison@consultant.aruba.it>
2025-01-07 09:33:21 +0100

    added common

--- inheritance/src/main/java/com/fdifrison/configurations/Profiles.java
+++ inheritance/src/main/java/com/fdifrison/configurations/Profiles.java
@@ -5,9 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "spring.profiles")
 public record Profiles(Active active) {
     public enum Active {
-        many2one,
-        one2many,
-        one2one,
-        many2many
+        single_table,
+        joined,
+        table_per_class,
+        mapped_superclass,
     }
 }
