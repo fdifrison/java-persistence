896255017f73a30b399d17d0fa0e69d902a8a26a
fdifrison <ing.giovanni.frison@gmail.com>
2025-02-27 10:53:08 +0100

    init projection project

---
+++ projections/src/main/java/com/fdifrison/configurations/Profiles.java
@@ -0,0 +1,10 @@
+package com.fdifrison.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.profiles")
+public record Profiles(Active active) {
+    public enum Active {
+        projection
+    }
+}
